d6618c52b05ee38e4c3b9a801ed6989f825033e3
ihesah1 <Hesah.Shaman.elq.js@tuwaiq.edu.sa>
2021-11-17 02:45:58 +0300

    add file

--- backendtodo/index.js
+++ backendtodo/index.js
@@ -11,21 +11,34 @@ dotenv.config();
 
 
 
-
-
-
-
-
 //built in level midleware
 app.use(express.json());
 app.use(cors0());
 app.use(morgan("dev"));
-
+const todos = [
+        {id:1,task:"sleep"},
+        {id:2, task: "eat"},
+        {id:3, task: "code"}
+        ]
 //middlewares
+app.get("/todos",async(req,res)=>{ ///////read
+ res.status(200)
+ res.json(todos)
+    });
+////create    
+ app.post("/create",(req,res)=>{
+    const{id,task}= req.body
+     //const newTask ={id: req.body.id, task:req.body.task} >> jsx
+     todos.push({id,task});
+            res.status(201);
+            res.json({id,task})
+        })
+
+})
 
 //routers
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, ()=> {
 console.log(`server on ${PORT}`);
 })
